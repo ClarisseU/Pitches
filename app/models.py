e1246ce595ae9899075c9bf3994dcdbbe95b86d4
ClarisseU <clarisseum@gmail.com>
2019-09-25 13:09:13 +0200

    add user class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,36 @@
 from . import db
 
+class User(UserMixin,db.Model):
+    __tablename__= 'users'
+    
+    id =db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    password_hash = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    pitch = db.relationship('Pitch',backref = 'users',lazy="dynamic")
+    comment = db.relationship('Comment', backref ='comments',lazy ="dynamic")
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+            
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+            
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
 class Pitch:
     '''
     Pitch class to define pitch objects
