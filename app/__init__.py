612bf08943b583382bec26225451cd76590c0608
ClarisseU <clarisseum@gmail.com>
2019-09-25 15:27:33 +0200

    upload photo

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,14 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
 
     app = Flask(__name__)
@@ -25,6 +26,9 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
+    # configure UploadSet
+    configure_uploads(app,photos)
     
     #...
     from .auth import auth as auth_blueprint
