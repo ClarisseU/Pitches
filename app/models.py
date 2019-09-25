82c172e2934b67fc0de785e76aa459fb2caecde1
ClarisseU <clarisseum@gmail.com>
2019-09-25 12:39:18 +0200

    add some changes

--- app/models.py
+++ app/models.py
@@ -2,11 +2,47 @@ class Pitch:
     '''
     Pitch class to define pitch objects
     '''
-    def __init__(self, id,name,category,upvote,downvote,publishedAt):
-        self.id = id
-        self.name = name
-        self.category = category
-        self.upvote = upvote
-        self.downvote = downvote
-        self.publishedAt = publishedAt
+    __tablename__='pitch'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    content =  db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    title = db.Column(db.String(255))
+    category = db.Column(db.Integer, db.ForeignKey('category.id'))
+    comments = db.relationship('Comment',backref = 'pitches', lazy ="dynamic")
+    publishedAt = db.Column(db.DateTime,default=datetime.utcnow)
+    upvotes = db.Column(db.Integer)
+    downvotes = db.Column(db.Integer)
+    
+    def save_pitches(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    @classmethod
+    def clear_pitch(cls):
+        Pitch.all_pitches.clear()
+        
+    def get_pitch(id):
+        pitch = Pitch.query.filter_by(category=id).all_pitches
+        return pitch
+    
+    @classmethod
+    def count_pitches(cls,uname):
+           user = User.query.filter_by(username=uname).first()
+        pitches = Pitch.query.filter_by(user_id=user.id).all()
+        
+        pitches_count = 0
+        for pitch in pitches:
+            pitches_count += 1
+            
+        return pitches_count
+    
+     def __repr__(self):
+        return f'Pitch {self.id}'
+    
+    
+class Category(db.model):
+        
+            
+            
         
