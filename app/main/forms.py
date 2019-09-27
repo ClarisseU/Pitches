884eafd55d39dfd004a54f891448ddab3b4ab27e
ClarisseU <clarisseum@gmail.com>
2019-09-27 10:34:04 +0200

    add some changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,11 +3,6 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField,SubmitField
 from wtforms.validators import Required
 from .. import db
-
-#update profile form
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
     
 #Pitch Form
 class PitForm(FlaskForm):
